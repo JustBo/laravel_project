dc8cb0c8c4b3168bde846700e06898b40adb392c
justbo <b.tsybulskyy@gmail.com>
2017-10-25 16:33:47 +0300

    22

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -18,52 +18,46 @@ class ArticlesController extends Controller
     }
 
     public function index(){
-
-      //return \Auth::user();
       $articles = Article::latest('published_at')->published()->get();
       return view('articles.articles', compact('articles'));
-
     }
 
     public function show( $id ){
-
       $article = Article::findOrFail( $id );
       return view('articles.article', compact('article') );
-
     }
 
     public function create(){
-
       $categories = Category::lists('name', 'id');
-
       return view('articles.create', compact('categories'));
-
     }
 
     public function store( ArticleRequest $request ){
-
-      //$input = Request::all();
-      // $article = new Article( $request->all() );
-      $article = \Auth::user()->articles()->create( $request->all() );
-      $article->categories()->attach( $request->input('categories_list') );
+      $article = $this->createArticle( $request );
       return redirect('articles');
-
     }
 
     public function edit( $id ){
-
       $categories = Category::lists('name', 'id');
       $article = Article::findOrFail( $id );
       return view('articles.edit', compact('article','categories') );
-
     }
 
     public function update( $id, ArticleRequest $request ){
-
       $article = Article::findOrFail( $id );
       $article->update($request->all());
+      $this->syncCategories( $article, $request->input('categories_list') );
       return redirect('articles');
+    }
 
+    private function syncCategories( Article $article, array $tags ){
+      $article->categories()->sync( $tags );
+    }
+
+    private function createArticle( ArticleRequest $request ){
+      $article = \Auth::user()->articles()->create( $request->all() );
+      $this->syncCategories( $article, $request->input('categories_list') );
+      return $article;
     }
 
 }
