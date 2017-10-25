7739c1a364650c6c7a3e2d5b23e7c0ce2fc17b2b
justbo <b.tsybulskyy@gmail.com>
2017-10-25 17:26:19 +0300

    navbar

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -7,6 +7,30 @@
     @yield('styles')
   </head>
   <body>
+
+    <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Blog</a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            {{-- <li><a href="#">Link <span class="sr-only">(current)</span></a></li> --}}
+            <li><a href="{{ url('/articles') }}">Articles</a></li>
+            <li><a href="{{ url('/about') }}">About</a></li>
+          </ul>
+        </div>
+      </div>
+    </nav>
     <div class="content">
       <div class="container">
         @yield('content')
